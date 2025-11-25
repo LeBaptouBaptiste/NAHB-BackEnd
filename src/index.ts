23009982e64fe6c0187ee37c78f3beabb3f308b4
LeBaptouBaptiste <baptiste.vidal.2005@gmail.com>
2025-11-25 15:45:10 +0100

    Suppression packageLock et cors/helmet

--- src/index.ts
+++ src/index.ts
@@ -22,13 +22,9 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(express.json());
-app.use(cors({
-    origin: ['http://localhost:5173', 'http://localhost:5174'],
-    credentials: true,
-}));
-app.use(helmet({
-    crossOriginResourcePolicy: { policy: "cross-origin" }
-}));
+app.use(cors());
+app.use(helmet());
+
 app.use('/uploads', express.static('public/uploads'));
 
 // Routes
